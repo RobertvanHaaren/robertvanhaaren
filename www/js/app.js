1202b0d110984d0868b3adc95c63d532210f17e8
Koen Vendrik <k.vendrik@gmail.com>
2016-05-15 16:43:58 +0200

    Moves onscreentoggle attr logic to seperate file

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,13 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-var dataOnScreenToggle = {
+exports.default = {
 	init: function init() {
 		var els = this._els = document.querySelectorAll('[data-onscreen-toggle]');
 
@@ -54,6 +58,15 @@ var dataOnScreenToggle = {
 	}
 };
 
-dataOnScreenToggle.init();
+},{}],2:[function(require,module,exports){
+'use strict';
 
-},{}]},{},[1]);
+var _dataOnScreenToggle = require('./dataOnScreenToggle');
+
+var _dataOnScreenToggle2 = _interopRequireDefault(_dataOnScreenToggle);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_dataOnScreenToggle2.default.init();
+
+},{"./dataOnScreenToggle":1}]},{},[2]);
