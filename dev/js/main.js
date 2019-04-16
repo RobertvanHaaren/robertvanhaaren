4dee15b84dea491cffee8af3c1cf7c207e3b69ac
Koen Vendrik <k.vendrik@gmail.com>
2019-04-16 14:38:27 -0400

    fixes on screen toggle

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,5 @@
 import initImageModal from './image-modal';
+import dataOnScreenToggle from './data-on-screen-toggle';
 
+dataOnScreenToggle.init();
 initImageModal();
--- www/js/app.js
+++ www/js/app.js
@@ -2,6 +2,70 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+exports.default = {
+	init: function init() {
+		var _this = this;
+
+		var els = this._els = document.querySelectorAll('[data-onscreen-toggle]');
+
+		if (els.length > 0) {
+			this._checkEls = this._checkEls.bind(this);
+
+			window.addEventListener('load', function () {
+				_this._checkEls();
+				document.addEventListener('scroll', _this._checkEls, false);
+			}, false);
+		}
+	},
+	_checkEls: function _checkEls() {
+		var _this2 = this;
+
+		[].concat(_toConsumableArray(this._els)).forEach(function (el) {
+			//check if element is on screen
+			var onScreen = _this2._isOnScreen(el);
+
+			if (onScreen) {
+				//if element is on screen
+				//add class
+				var addClassName = el.getAttribute('data-onscreen-toggle');
+				el.classList.add(addClassName);
+			}
+		});
+	},
+	_isOnScreen: function _isOnScreen(el) {
+		var win = window;
+
+		//adds an extra offset so the class isn't added
+		//untill its completely in the viewport
+		var extraOffset = el.offsetHeight - el.offsetHeight / 1.5;
+
+		var viewport = {
+			top: win.scrollY,
+			left: win.scrollX
+		};
+		viewport.right = viewport.left + win.innerWidth;
+		viewport.bottom = viewport.top + win.innerHeight;
+
+		var bounds = {
+			top: el.offsetTop + extraOffset,
+			left: el.offsetLeft
+		};
+		bounds.right = bounds.left + el.offsetWidth;
+		bounds.bottom = bounds.top + el.offsetHeight;
+
+		return !(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom);
+	}
+};
+
+},{}],2:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = initImageModal;
@@ -27,15 +91,20 @@ function initImageModal() {
   });
 }
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 'use strict';
 
 var _imageModal = require('./image-modal');
 
 var _imageModal2 = _interopRequireDefault(_imageModal);
 
+var _dataOnScreenToggle = require('./data-on-screen-toggle');
+
+var _dataOnScreenToggle2 = _interopRequireDefault(_dataOnScreenToggle);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+_dataOnScreenToggle2.default.init();
 (0, _imageModal2.default)();
 
-},{"./image-modal":1}]},{},[2]);
+},{"./data-on-screen-toggle":1,"./image-modal":2}]},{},[3]);
